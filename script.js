54fd914eb53e9eb0519f776d4a7c8e60de80c2cd
Mehul Shah <164782550+MEEHHUULL@users.noreply.github.com>
2024-04-11 22:30:08 +0100

    styling done.

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(a,b) {
+    return a + b;
+}
+
+function minus(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+let a = "" ; 
+let b = "" ; 
+const multiply = 
+const divide = 
+const add = 
+const minus =
